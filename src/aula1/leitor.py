27f46cc53fc34d73165da82b6710158d2c23a247
Dan <danper.jesus@gmail.com>
2018-07-18 19:15:52 -0300

    Movendo as pastas das aulas para a pasta src

--- src/aula1/leitor.py
+++ src/aula1/leitor.py
@@ -3,6 +3,11 @@ import json
 with open('company.json') as f:
     company = json.load(f)
 
-print(company)
+#print(company)
 
-print(company['cnpj'])
+#print(company['cnpj'])
+
+#print(company['tag1']['tag2'])
+
+for key, value in company.items():
+    print(key, value)
